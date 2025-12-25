68a1a0859a83e0599adb39b7eb1566f6124f1a9c
Darshana Ujad <darshana.ujad@gmail.com>
2025-12-25 18:58:16 +0530

    added changes

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+
+  const navigate = useNavigate();
   return (
     <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-12'>
-      <img src={assets.logo} alt="logo" className='w-32 sm:w-44'/>
-      <button className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-30 py-2.5'>
+      <img onClick={()=>navigate('/')} src={assets.logo} alt="logo" className='w-32 sm:w-44 cursor-pointer'/>
+      <button onClick={()=>navigate('/admin')} className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-30 py-2.5'>
         Login 
         <img src={assets.arrow}  className='w-3' alt="arrow" />
       </button>
