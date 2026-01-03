69df4f078022ed741dc6edcc3543fc5ed6a7165d
Darshana Ujad <darshana.ujad@gmail.com>
2026-01-03 17:00:45 +0530

    added

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { blog_data } from '../assets/assets';
 
 const Blog = () => {
+  const{id} = useParams()
+
+  const [data , setdata] = useState(null);
+
+  const fetchBlogData = async()=>{
+    blog_data
+  }
   return (
     <div>
       Blog
